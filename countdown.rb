a7e32db9fc5323665f8ffc577accb8ab461b938e
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 03:53:48 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown (number)
-  while number > 0 do 
+  while number > 0 do
     puts  "#{number} SECOND(S)!"
     number -= 1
 end
@@ -9,9 +9,9 @@ end
 end
 
 def countdown_with_sleep(number)
-  while number > 0 do 
+  while number > 0 do
       puts  "#{number} SECOND(S)!"
-    sleep 1 
+    sleep 1
     number -= 1
   end
 end 
